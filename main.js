7cd27ed01101e82ec2ebdf74e528dd84d044782b
Himadri Sekhar Ray <himadri.1111@gmail.com>
2022-02-07 12:30:46 +0530

    assignment with 3 end point

--- main.js
+++ main.js
@@ -61,12 +61,77 @@ app.get('/salesrep', function (req, res) {
 
     }
      res.send(responseArray);
-
-
-  })
-  
+  })  
 })
 
+app.get('/optimal', function (req, res) {
+  axios.get('http://localhost:3000/countries/')
+  .then(function(response) {
+    var allCountries = response.data;
+    var countriesByRegion= [];
+    for(var i in allCountries){
+      if( countriesByRegion[allCountries[i].region] === undefined ) {
+          countriesByRegion[allCountries[i].region] = [];
+          countriesByRegion[allCountries[i].region].push(allCountries[i].name);
+      }
+      else{
+        countriesByRegion[allCountries[i].region].push(allCountries[i].name);
+      }      
+    }
+    var responseArray = [];
+    var maxCountry = 7;
 
+    for(var i in countriesByRegion){
+      var counrtyCount = 1;
+      var tempCountryList = [];
+
+      var k = 0;
+      for(var j in countriesByRegion[i]){
+
+        tempCountryList.push(countriesByRegion[i][j]);       
+
+        if(counrtyCount == 7 ){
+          responseArray.push({region:i, countryList: tempCountryList, counrtyCount: tempCountryList.length});
+          tempCountryList = [];
+          counrtyCount = 0
+        }
+
+        if(k == (countriesByRegion[i].length-1)){
+          responseArray.push({region:i, countryList: tempCountryList, counrtyCount: tempCountryList.length});
+          tempCountryList = []; 
+          counrtyCount = 1         
+        }
+        counrtyCount++;
+        k++;
+      }
+    }
+     res.send(responseArray);
+  })
+
+})
+
+/*
+app.get('/region_countries', function (req, res) {
+  var responseArray = [];
+  axios.get('http://localhost:3000/countries/')
+  .then(function(response) {
+    var allCountries = response.data;
+    var countriesByRegion= [];
+    for(var i in allCountries){
+      if( countriesByRegion[allCountries[i].region] === undefined ) {
+          countriesByRegion[allCountries[i].region] = [];
+          countriesByRegion[allCountries[i].region].push(allCountries[i].name);
+      }
+      else{
+        countriesByRegion[allCountries[i].region].push(allCountries[i].name);
+      }      
+    }
+    
+    console.log(countriesByRegion);
+    responseArray.push(countriesByRegion);
+    
+  })
+  res.send(responseArray);
+})*/
 
 app.listen(3000, () => console.log('Server Started at port 3000'))
